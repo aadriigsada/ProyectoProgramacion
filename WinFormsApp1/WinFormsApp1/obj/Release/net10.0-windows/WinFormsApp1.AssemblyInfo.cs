298ac4930f10d192ae1dbecd1a20be512ba418a5
aadriigsada <agassku9j@adaits.es>
2026-04-30 13:33:49 +0200

    actualizacion
    Actializacion proyecto

--- WinFormsApp1/WinFormsApp1/obj/Release/net10.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Release/net10.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a36f7b9c496a851ec13adad9e5e82fbf11b0cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
